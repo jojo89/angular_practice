12b81605c3a03ffc9a1c984a8f9414640fabebf6
jojo89 <burtoftallbiz@gmail.com>
2013-12-11 16:12:04 -0600

    fixed strong params bug

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,17 +1,17 @@
 class EntriesController < ApplicationController
   respond_to :json
-
+  skip_before_filter  :verify_authenticity_token
   def index
     respond_with Entry.all
   end
 
   def show
-  	
+
     respond_with Entry.find(params[:id])
   end
 
   def create
-    respond_with Entry.create(params[:entry])
+    respond_with Entry.create(name: params[:entry][:name])
   end
 
   def update
@@ -21,6 +21,8 @@ class EntriesController < ApplicationController
   def destroy
     respond_with Entry.destroy(params[:id])
   end
+
+
 end
 
 
